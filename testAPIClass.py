136e313f929c16acaa2e0373197f91bbfd20ed50
sthegai <106828817+sthegai@users.noreply.github.com>
2024-03-13 14:23:05 -0400

    Update testAPIClass.py

--- testAPIClass.py
+++ testAPIClass.py
@@ -1,14 +1,43 @@
-import requests
-import json
+from fastapi import FastAPI
+
+from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException, Path
+
+app = FastAPI()
+api_keys_to_files = {
+    "csvtest": r'C:\Users\16479\PycharmProjects\COMP-3220\SampleCSVFile_556kb.csv',
+    "xmltest": r'C:\Users\16479\PycharmProjects\COMP-3220\TestConfigInNamespace.xml',
+    # Add more keys and file paths as needed
+}
 
 
-def aircraft_api_call():
-    response = requests.get(
-        'https://open.canada.ca/data/dataset/98abeb62-7c76-4dfb-a134-1551f55ede55/resource/1b977865-6f74-4548-b024-d6ca1a6161a3/download/adminaircraft.json')
+@app.get('/')
+async def root():
+    return {'Webpage'}
 
-    return response.json()
+@app.get('/api/csv/test')
+async def download_csv():
+    file_path = r'C:\Users\16479\PycharmProjects\COMP-3220\SampleCSVFile_556kb.csv'  # Update this to the actual path of your CSV file
+    return FileResponse(path=file_path, filename="SampleCSVFile_556kb.csv", media_type='text/csv')
 
+@app.get('/api/xml/test')
+async def download_csv():
+    file_path = r'C:\Users\16479\PycharmProjects\COMP-3220\TestConfigInNamespace.xml'  # Update this to the actual path of your CSV file
+    return FileResponse(path=file_path, filename="TestConfigInNamespace.xml", media_type='application/xml')
 
-def csv_api_call():
-    response = requests.get('https://data.novascotia.ca/api/views/w64p-5ue3/rows.csv?accessType=DOWNLOAD')
-    return response.json()
+@app.get('/api/{api_key}')
+async def download_csv(api_key: str):
+
+    if api_key in api_keys_to_files:
+        file_path = api_keys_to_files[api_key]
+        filename = file_path.split('\\')[-1]
+
+        if file_path.lower().endswith('.csv'):
+            media_type = 'text/csv'
+        elif file_path.lower().endswith('.xml'):
+            media_type = 'application/xml'
+        else:
+            raise HTTPException(status_code=404, detail="Type not supported.")
+
+        return FileResponse(path=file_path, filename=filename, media_type=media_type)
+    raise HTTPException(status_code=404, detail="File not found for the provided API key.")
