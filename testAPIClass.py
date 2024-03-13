a3d4614ee323336cafc19b111fde25527fd59868
sthegai <106828817+sthegai@users.noreply.github.com>
2024-03-13 15:44:48 -0400

    Update testAPIClass.py

--- testAPIClass.py
+++ testAPIClass.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI
 
 from fastapi.responses import FileResponse
-from fastapi import FastAPI, HTTPException, Path
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 api_keys_to_files = {
-    "csvtest": 'SampleCSVFile_556kb.csv',
-    "xmltest": 'TestConfigInNamespace.xml',
+    "csvtest": r'C:\Users\16479\PycharmProjects\COMP-3220\SampleCSVFile_556kb.csv',
+    "xmltest": r'C:\Users\16479\PycharmProjects\COMP-3220\TestConfigInNamespace.xml',
     # Add more keys and file paths as needed
 }
 
@@ -14,16 +13,6 @@ api_keys_to_files = {
 @app.get('/')
 async def root():
     return {'Webpage'}
-
-@app.get('/api/csv/test')
-async def download_csv():
-    file_path = r'C:\Users\16479\PycharmProjects\COMP-3220\SampleCSVFile_556kb.csv'  # Update this to the actual path of your CSV file
-    return FileResponse(path=file_path, filename="SampleCSVFile_556kb.csv", media_type='text/csv')
-
-@app.get('/api/xml/test')
-async def download_csv():
-    file_path = r'C:\Users\16479\PycharmProjects\COMP-3220\TestConfigInNamespace.xml'  # Update this to the actual path of your CSV file
-    return FileResponse(path=file_path, filename="TestConfigInNamespace.xml", media_type='application/xml')
 
 @app.get('/api/{api_key}')
 async def download_csv(api_key: str):
